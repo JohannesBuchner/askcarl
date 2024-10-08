994b4b8673e6b82cc9b957416214f52181a56326
Johannes Buchner <johannes.buchner.acad@gmx.com>
2024-10-08 20:15:25 +0200

    Bump version: 0.2.0 → 0.2.1

--- askcarl/__init__.py
+++ askcarl/__init__.py
@@ -1,5 +1,5 @@
 """Multivariate Gaussians with support for upper limits and missing data."""
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 from .gaussian import Gaussian, pdfcdf
 from .mixture import GaussianMixture
--- setup.py
+++ setup.py
@@ -51,5 +51,5 @@ setup(
     tests_require=test_requirements,
     extras_require=dict(plot=['matplotlib', 'scipy']),
     url='https://github.com/JohannesBuchner/askcarl',
-    version='0.2.0',
+    version='0.2.1',
 )
