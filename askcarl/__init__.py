923158e9b580d583965c7be6d5468daa13ce46ef
Johannes Buchner <johannes.buchner.acad@gmx.com>
2025-05-31 16:24:29 +0200

    Bump version: 0.2.1 → 1.0.0

--- askcarl/__init__.py
+++ askcarl/__init__.py
@@ -1,5 +1,5 @@
 """Multivariate Gaussians with support for upper limits and missing data."""
 
-__version__ = '0.2.1'
+__version__ = '1.0.0'
 from .gaussian import Gaussian, pdfcdf
 from .mixture import GaussianMixture
