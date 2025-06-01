bb28728a3aee3c51be561d98f63b296a79b4c552
Johannes Buchner <johannes.buchner.acad@gmx.com>
2025-06-01 10:05:42 +0200

    Bump version: 1.2.0 → 1.2.1

--- setup.py
+++ setup.py
@@ -51,5 +51,5 @@ setup(
     tests_require=test_requirements,
     extras_require=dict(plot=['matplotlib', 'scipy']),
     url='https://github.com/JohannesBuchner/askcarl',
-    version='1.2.0',
+    version='1.2.1',
 )
