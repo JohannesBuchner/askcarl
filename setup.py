21ee2ac92b306efe5e7fdbe269c9c52b6b787005
Johannes Buchner <johannes.buchner.acad@gmx.com>
2025-10-28 12:10:02 +0200

    Bump version: 3.0.0 → 3.0.1

--- setup.py
+++ setup.py
@@ -54,5 +54,5 @@ setup(
     tests_require=test_requirements,
     extras_require=dict(plot=['matplotlib', 'scipy']),
     url='https://github.com/JohannesBuchner/askcarl',
-    version='3.0.0',
+    version='3.0.1',
 )
